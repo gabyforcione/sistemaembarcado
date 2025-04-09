5558788e346daa7edd7329bf3f0de916498f1f42
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-04-09 23:22:38 +0000

    Sistema de Batimentos Cardíacos Atualizado + Observação sobre a função

--- Batimentos_Cardíacos.py
+++ Batimentos_Cardíacos.py
@@ -3,6 +3,11 @@ import random
 import time
 
 def Checar_Batimentos():
+# Esta função deve checar os batimentos cardíacos a cada 5 segundos; de acordo com minhas pesquisas, eu coloquei como se
+# os batimentos estiverem maiores ou iguais a 120, apresenta sinal de perigo; menores que 60, apresenta sinal de perigo;
+# se estiverem igual a 50 ou 180, você morreu; e se não for nenhum desses (estiver entre 61 e 119), está tudo normal.
+# Também, quando há sinal de perigo ele mostra que o marcapasso foi ativado.
+
     while True:
         batimentos = random.uniform(50, 180)
         if batimentos >= 120:
