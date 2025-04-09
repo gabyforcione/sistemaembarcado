4c51ae9d8ea624d3b348607b5c35ef54ec669316
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-04-09 23:11:59 +0000

    Atualizações no Sistema de Batimentos Cardíacos

--- Batimentos_Cardíacos.py
+++ Batimentos_Cardíacos.py
@@ -4,12 +4,14 @@ import time
 
 def Checar_Batimentos():
     while True:
-        batimentos = random.uniform(0, 50)
-        if batimentos >= 25:
-          print("Os batimentos estão em ",int(batimentos),"; estão muito rápidos, tome cuidado.")
-        elif batimentos <= 10:
-          print("Os batimentos estão em ",int(batimentos),"; estão muito lentos, tome cuidado.")
-        elif batimentos == 0:
+        batimentos = random.uniform(50, 180)
+        if batimentos >= 120:
+          print("Os batimentos estão em ",int(batimentos),"; estão muito rápidos, marcapasso ativado.")
+        elif batimentos < 60:
+          print("Os batimentos estão em ",int(batimentos),"; estão muito lentos, marcapasso ativado.")
+        elif batimentos == 50:
+          print("Sinto muito, mas você não resistiu...")
+        elif batimentos == 180:
           print("Sinto muito, mas você não resistiu...")
         else:
           print("Tudo normal com seus batimentos, estão em ",int(batimentos),".")
